ff99d92c7c79c3be7a24772f4b125f4ebd9d59d5
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-02-06 02:27:44 +0100

    gonna test this to see if a smaller keyboard is cool

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -33,6 +33,8 @@
 #define RALT_DEL RALT_T(KC_DEL)
 //CTRL on hold, ' on tap
 #define CTL_QT CTL_T(NO_QUOT)
+//LGUI on hold, Tab on tap
+#define GUI_TAB LGUI_T(KC_TAB)
 
 //Layers
 #define L1 0
@@ -91,11 +93,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L1] = LAYOUT(
-	KC_LGUI,       KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                                     KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_RGUI,
-	KC_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         OSM_LALT,
+	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+	GUI_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         OSM_LALT,
 	KC_BSPC,       KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         NO_SCLN,      CTL_QT,
 	OSM_LSFT,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         KC_COMM,      KC_DOT,       NO_SLSH,      OSM_RSFT,
-	OSM_LCTL,      RALT_DEL,     OSM_LALT,     NO_MINS,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       MO_L4,        NO_MINS,      OSM_LALT,     NO_PLUS,      OSM_RCTL
+	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       MO_L4,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX, 
   ),
 
   [L2] = LAYOUT(
@@ -107,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [L3] = LAYOUT(
-	XXXXXXX,       SS_QTS,       SS_DQTS,      XXXXXXX,      SS_BRCS,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      SS_CBRS,      XXXXXXX,      SS_POPC,      XXXXXXX,
+	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
 	XXXXXXX,       KC_ESC,       NO_COLN,      NO_AT,        NO_QUOT,      NO_TILD,                                  NO_QUES,      NO_EXLM,      NO_PIPE,      NO_AND,       NO_PLUS,      _______,
 	_______,       NO_ASTR,      NO_LBRC,      NO_PO,        NO_PC,        NO_RBRC,                                  NO_HAT,       NO_RCBR,      NO_LCBR,      NO_DLR,       KC_TAB,       _______,
 	_______,       NO_SECT,      NO_BSLS,      NO_PERC,      NO_DQT,       NO_GRV,                                   NO_MINS,      NO_EQL,       NO_GT,        NO_LT,        NO_HASH,      _______,
@@ -115,11 +117,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [L4] = LAYOUT(
-	XXXXXXX,       TO(L1),       TO(L2),       TO(L3),       TO(L4),       XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	KC_DEL,        KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,                                    KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       XXXXXXX,      XXXXXXX,
-	KC_BSPC,       KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,                                   KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     _______,      XXXXXXX,
-        KC_CAPS,       KC_F11,       KC_F12,       XXXXXXX,      QK_RBT,       QK_BOOT,                                  KC_MS_L,      KC_MS_D,      KC_MS_U,      KC_MS_R,      XXXXXXX,      XXXXXXX,
-        OSM_LCTL,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BTN1,      KC_BTN2,      KC_BTN3,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	KC_F1,         KC_F2,        KC_F3,        KC_F4,        KC_F5,        KC_F6,                                    KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       XXXXXXX,      XXXXXXX,
+	KC_F7,         KC_F8,        KC_F9,        KC_F10,       KC_F11,       KC_F12,                                   KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     _______,      XXXXXXX,
+        KC_LSFT,       KC_1,         KC_2,         KC_3,         KC_4,         KC_4,                                     KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_RSFT,
+        XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BTN1,      KC_BTN2,      KC_BTN3,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
 };
