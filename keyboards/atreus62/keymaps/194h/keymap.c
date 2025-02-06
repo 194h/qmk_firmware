85e8665a16949f37a8474b58d945b1bf4de4cefa
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-02-06 02:28:07 +0100

    gonna test this to see if a smaller keyboard is cool

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -1,21 +1,10 @@
 #include QMK_KEYBOARD_H
-#include "keymap_norwegian.h"
-#include "sendstring_norwegian.h"
 
-//Fix norwegian key names, and add some symbols
-//Change names
-#define NO_DQT NO_DQUO
-#define NO_AO NO_ARNG
-#define NO_OE NO_OSTR
-#define NO_AND NO_AMPR
-#define NO_PO NO_LPRN
-#define NO_PC NO_RPRN
-#define NO_HAT NO_CIRC
-#define NO_GT NO_LABK
-#define NO_LT NO_RABK
-//Add symbols
-#define  NO_EN    RALT(NO_MINS)   //  –
-#define  NO_EM    LSFT(RALT(NO_MINS))  //  —
+//Some of the default aliases make no sense
+#define KC_AND KC_AMPERSAND
+#define KC_PO KC_LEFT_PAREN
+#define KC_PC KC_RIGHT_PAREN
+#define KC_HAT KC_CIRCUMFLEX
 
 //One Shot Modifier
 #define OSM_LCTL OSM(MOD_LCTL)
@@ -27,101 +16,84 @@
 #define OSM_RSFT OSM(MOD_RSFT)
 #define OSM_RGUI OSM(MOD_RGUI)
 
-//CTRL on hold, ESC on tap
-#define CTL_ESC CTL_T(KC_ESC)
-//RALT on hold, DEL on tap
-#define RALT_DEL RALT_T(KC_DEL)
-//CTRL on hold, ' on tap
-#define CTL_QT CTL_T(NO_QUOT)
-//LGUI on hold, Tab on tap
-#define GUI_TAB LGUI_T(KC_TAB)
+//CTRL on hold, F on tap
+#define CTL_F LCTL_T(KC_F)
+//CTRL on hold, J on tap
+#define CTL_J RCTL_T(KC_J)
+//SHIFT on hold, D on tap
+#define SFT_D LSFT_T(KC_D)
+//SHIFT on hold, D on tap
+#define SFT_K RSFT_T(KC_K)
+//ALT on hold, A on tap
+#define ALT_A LALT_T(KC_A)
+//ALT on hold, ; on tap
+#define ALT_SCL LALT_T(KC_SCLN)
+//LGUI on hold, G on tap
+#define GUI_G LGUI_T(KC_G)
+//RGUI on hold, H on tap
+#define GUI_H RGUI_T(KC_H)
+//RALT on hold, Z on tap
+#define RALT_Z RALT_T(KC_Z)
 
 //Layers
 #define L1 0
 #define L2 1
 #define L3 2
 #define L4 3
+#define L5 4
 
 // Momentary switch to layer
 #define MO_L2 MO(L2)
 #define MO_L4 MO(L4)
 // Momentary switch to layer - One Shot Layer
-#define OSL_L2 OSL(L2)
 #define OSL_L3 OSL(L3)
 #define OSL_L4 OSL(L4)
 
-enum custom_keycodes {
-    SS_CBRS = SAFE_RANGE,
-    SS_BRCS,
-    SS_POPC,
-    SS_QTS,
-    SS_DQTS,
-    NEW_SAFE_RANGE
-};
+// Layer tap
+#define LT5_L LT(L5, KC_L)
+#define LT5_S LT(L5, KC_S)
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case SS_CBRS:
-            if (record->event.pressed) {
-                SEND_STRING("{}" SS_TAP(X_LEFT));
-            }
-            return false;
-        case SS_BRCS:
-            if (record->event.pressed) {
-                SEND_STRING("[]" SS_TAP(X_LEFT));
-            }
-            return false;
-        case SS_POPC:
-            if (record->event.pressed) {
-                SEND_STRING("()" SS_TAP(X_LEFT));
-            }
-            return false;
-        case SS_QTS:
-            if (record->event.pressed) {
-                SEND_STRING("''" SS_TAP(X_LEFT));
-            }
-            return false;
-        case SS_DQTS:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LSFT("22") SS_TAP(X_LEFT));
-            }
-            return false;
-    }
-    return true;
-}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L1] = LAYOUT(
-	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	GUI_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         OSM_LALT,
-	KC_BSPC,       KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         NO_SCLN,      CTL_QT,
-	OSM_LSFT,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         KC_COMM,      KC_DOT,       NO_SLSH,      OSM_RSFT,
-	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       MO_L4,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX, 
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+	KC_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_EQL,
+	KC_BSPC,      ALT_A,        LT5_S,        SFT_D,        CTL_F,        GUI_G,                                    GUI_H,        CTL_J,        SFT_K,        LT5_L,        ALT_SCL,      KC_QUOT,
+	KC_ESC,       RALT_Z,       KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      KC_MINS,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       MO_L4,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX 
   ),
 
   [L2] = LAYOUT(
-	_______,       _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      _______,
-	_______,       _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      NO_AO,
-	_______,       _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      NO_OE,        NO_AE,
-	_______,       _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      _______,
-	_______,       _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
+	_______,      _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      _______,
+	_______,      _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      _______,
+	_______,      _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      _______,
+	_______,      _______,      _______,      _______,      _______,      _______,                                  _______,      _______,      _______,      _______,      _______,      _______,
+	_______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
   ),
 
   [L3] = LAYOUT(
-	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	XXXXXXX,       KC_ESC,       NO_COLN,      NO_AT,        NO_QUOT,      NO_TILD,                                  NO_QUES,      NO_EXLM,      NO_PIPE,      NO_AND,       NO_PLUS,      _______,
-	_______,       NO_ASTR,      NO_LBRC,      NO_PO,        NO_PC,        NO_RBRC,                                  NO_HAT,       NO_RCBR,      NO_LCBR,      NO_DLR,       KC_TAB,       _______,
-	_______,       NO_SECT,      NO_BSLS,      NO_PERC,      NO_DQT,       NO_GRV,                                   NO_MINS,      NO_EQL,       NO_GT,        NO_LT,        NO_HASH,      _______,
-	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      NO_UNDS,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_AT,        KC_QUOT,      KC_TILD,                                  KC_QUES,      KC_EXLM,      KC_PIPE,      KC_AND,       XXXXXXX,      _______,
+	_______,      KC_ASTR,      KC_LBRC,      KC_PO,        KC_PC,        KC_RBRC,                                  KC_HAT,       KC_RCBR,      KC_LCBR,      KC_DLR,       XXXXXXX,      _______,
+	_______,      XXXXXXX,      KC_BSLS,      KC_PERC,      KC_DQT,       KC_GRV,                                   XXXXXXX,      KC_HASH,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_UNDS,      XXXXXXX,      KC_COLN,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
   [L4] = LAYOUT(
-	XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
-	KC_F1,         KC_F2,        KC_F3,        KC_F4,        KC_F5,        KC_F6,                                    KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       XXXXXXX,      XXXXXXX,
-	KC_F7,         KC_F8,        KC_F9,        KC_F10,       KC_F11,       KC_F12,                                   KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     _______,      XXXXXXX,
-        KC_LSFT,       KC_1,         KC_2,         KC_3,         KC_4,         KC_4,                                     KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_RSFT,
-        XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BTN1,      KC_BTN2,      KC_BTN3,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BTN1,      KC_BTN2,      XXXXXXX,                                  KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       XXXXXXX,      QK_BOOT,
+	_______,      XXXXXXX,      MS_LEFT,      MS_UP,        MS_DOWN,      MS_RGHT,                                  KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     XXXXXXX,      XXXXXXX,
+        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BTN3,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+  ),
+
+  [L5] = LAYOUT(
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_ESC,       XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
+	_______,      KC_1,         KC_2,         KC_3,         KC_4,         KC_0,                                     KC_0,         KC_1,         KC_2,         KC_3,         KC_4,         XXXXXXX,
+        XXXXXXX,      KC_5,         KC_6,         KC_7,         KC_8,         KC_9,                                     XXXXXXX,      KC_5,         KC_6,         KC_7,         KC_8,         KC_9,
+        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
 };
