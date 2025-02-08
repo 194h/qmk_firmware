40174fccc3b7de6862cfd760164466c7079ec565
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-02-08 08:41:40 +0100

    timing

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -6,32 +6,23 @@
 #define KC_PC KC_RIGHT_PAREN
 #define KC_HAT KC_CIRCUMFLEX
 
-//One Shot Modifier
-#define OSM_LCTL OSM(MOD_LCTL)
-#define OSM_LALT OSM(MOD_LALT)
-#define OSM_LSFT OSM(MOD_LSFT)
-#define OSM_LGUI OSM(MOD_LGUI)
-#define OSM_RCTL OSM(MOD_RCTL)
-#define OSM_RALT OSM(MOD_RALT)
-#define OSM_RSFT OSM(MOD_RSFT)
-#define OSM_RGUI OSM(MOD_RGUI)
+//CTRL on hold, D on tap
+#define CTL_D LCTL_T(KC_D)
+//CTRL on hold, K on tap
+#define CTL_K RCTL_T(KC_K)
+//SHIFT on hold, F on tap
+#define SFT_F LSFT_T(KC_F)
+//SHIFT on hold, J on tap
+#define SFT_J RSFT_T(KC_J)
+//ALT on hold, S on tap
+#define ALT_S LALT_T(KC_S)
+//ALT on hold, L on tap
+#define ALT_L LALT_T(KC_L)
+//LGUI on hold, A on tap
+#define GUI_A LGUI_T(KC_A)
+//RGUI on hold, ; on tap
+#define GUI_SCL RGUI_T(KC_SCLN)
 
-//CTRL on hold, F on tap
-#define CTL_F LCTL_T(KC_F)
-//CTRL on hold, J on tap
-#define CTL_J RCTL_T(KC_J)
-//SHIFT on hold, D on tap
-#define SFT_D LSFT_T(KC_D)
-//SHIFT on hold, D on tap
-#define SFT_K RSFT_T(KC_K)
-//ALT on hold, A on tap
-#define ALT_A LALT_T(KC_A)
-//ALT on hold, ; on tap
-#define ALT_SCL LALT_T(KC_SCLN)
-//LGUI on hold, G on tap
-#define GUI_G LGUI_T(KC_G)
-//RGUI on hold, H on tap
-#define GUI_H RGUI_T(KC_H)
 //RALT on hold, Z on tap
 #define RALT_Z RALT_T(KC_Z)
 
@@ -50,8 +41,8 @@
 #define OSL_L4 OSL(L4)
 
 // Layer tap
-#define LT5_L LT(L5, KC_L)
-#define LT5_S LT(L5, KC_S)
+#define LT5_G LT(L5, KC_G)
+#define LT5_H LT(L5, KC_H)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L1] = LAYOUT(
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
 	KC_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_EQL,
-	KC_BSPC,      ALT_A,        LT5_S,        SFT_D,        CTL_F,        GUI_G,                                    GUI_H,        CTL_J,        SFT_K,        LT5_L,        ALT_SCL,      KC_QUOT,
+	KC_BSPC,      GUI_A,        ALT_S,        CTL_D,        SFT_F,        LT5_G,                                    LT5_H,        SFT_J,        CTL_K,        ALT_L,        GUI_SCL,      KC_QUOT,
 	KC_ESC,       RALT_Z,       KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      KC_MINS,
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       MO_L4,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX 
   ),
@@ -74,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L3] = LAYOUT(
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_AT,        KC_QUOT,      KC_TILD,                                  KC_QUES,      KC_EXLM,      KC_PIPE,      KC_AND,       XXXXXXX,      _______,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_AT,        XXXXXXX,      KC_TILD,                                  KC_QUES,      KC_EXLM,      KC_PIPE,      KC_AND,       XXXXXXX,      _______,
 	_______,      KC_ASTR,      KC_LBRC,      KC_PO,        KC_PC,        KC_RBRC,                                  KC_HAT,       KC_RCBR,      KC_LCBR,      KC_DLR,       XXXXXXX,      _______,
-	_______,      XXXXXXX,      KC_BSLS,      KC_PERC,      KC_DQT,       KC_GRV,                                   XXXXXXX,      KC_HASH,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
-	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_UNDS,      XXXXXXX,      KC_COLN,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	_______,      XXXXXXX,      KC_BSLS,      KC_PERC,      XXXXXXX,      KC_GRV,                                   XXXXXXX,      KC_HASH,      XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_COLN,      KC_UNDS,      XXXXXXX,      KC_COLN,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
   [L4] = LAYOUT(
@@ -90,10 +81,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L5] = LAYOUT(
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_ESC,       XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	_______,      KC_4,         KC_3,         KC_2,         KC_1,         KC_0,                                     KC_0,         KC_1,         KC_2,         KC_3,         KC_4,         XXXXXXX,
-        KC_9,         KC_8,         KC_7,         KC_6,         KC_5,         XXXXXXX,                                  XXXXXXX,      KC_5,         KC_6,         KC_7,         KC_8,         KC_9,
-        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	XXXXXXX,      KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                                     KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         XXXXXXX,
+	_______,      KC_5,         KC_4,         KC_3,         KC_2,         KC_0,                                     KC_0,         KC_2,         KC_3,         KC_4,         KC_5,         XXXXXXX,
+        XXXXXXX,      KC_0,         KC_9,         KC_8,         KC_7,         XXXXXXX,                                  XXXXXXX,      KC_7,         KC_8,         KC_9,         KC_0,         XXXXXXX,
+        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_6,         KC_1,         XXXXXXX,      XXXXXXX,      KC_1,         KC_6,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case SFT_F:
+            return 150;
+        case SFT_J:
+            return 150;
+        case LT5_G:
+            return 150;
+        case LT5_H:
+            return 150;
+        default:
+            return TAPPING_TERM;  // Default fallback
+    }
+}
