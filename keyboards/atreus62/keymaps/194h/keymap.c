4facddc6640e3ee82eb7092b8e23f417e0e58c10
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-02-13 20:31:41 +0100

    maybe this makes more sense

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -16,8 +16,10 @@
 #define ALT_COM LALT_T(KC_COMM)
 //RCTL on hold, / on tap
 #define CTL_SLSH RCTL_T(KC_SLSH)
-//RGUI on hold, Tab on tap
-#define GUI_TAB RGUI_T(KC_TAB)
+//RGUI on hold, - on tap
+#define GUI_MIN LGUI_T(KC_MINS)
+//LGUI on hold, Tab on tap
+#define GUI_TAB LGUI_T(KC_TAB)
 //LCTL on hold, Z on tap
 #define CTL_Z LCTL_T(KC_Z)
 
@@ -39,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L1] = LAYOUT(
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	GUI_TAB,      KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_RGUI,
+	GUI_TAB,      KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         GUI_MIN,
 	KC_BSPC,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      RALT_QUOT,
 	OSM_LSFT,     CTL_Z,        KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         ALT_COM,      KC_DOT,       CTL_SLSH,     OSM_RSFT,
-	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       OSL_L4,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX 
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      MO_L4,        OSL_L3,       KC_SPC,       KC_ENT,       OSL_L3,       OSL_L4,       XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
   [L2] = LAYOUT(
@@ -63,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L4] = LAYOUT(
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
-	KC_LGUI,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       XXXXXXX,      KC_RGUI,
-	_______,      KC_5,         KC_4,         KC_3,         KC_2,         KC_1,                                     KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     XXXXXXX,      XXXXXXX,
-        KC_CAPS,      KC_0,         KC_9,         KC_8,         KC_7,         KC_6,                                     KC_RGUI,      KC_RGUI,      KC_RGUI,      KC_RGUI,      XXXXXXX,      XXXXXXX,
-        XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_6,         KC_1,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	KC_LGUI,      KC_0,         KC_9,         KC_8,         KC_7,         KC_6,                                     KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_F12,       KC_RGUI,
+	_______,      KC_5,         KC_4,         KC_3,         KC_2,         KC_1,                                     KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     KC_F11,       XXXXXXX,
+	KC_CAPS,      KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,                                    KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,       XXXXXXX,
+	XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_6,         KC_1,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
 };
