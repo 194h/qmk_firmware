3cd34e200c791b1d1735bf7ac0636d60c984c9b7
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-03-27 15:40:37 +0100

    direction MS_ACL

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -61,7 +61,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L1] = LAYOUT(
-	MS_BTN4,      MS_ACL0,      MS_ACL2,      MS_BTN3,      MS_BTN2,      MS_BTN1,                                  MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,      XXXXXXX,      MS_BTN5,
+	MS_BTN4,      MS_ACL2,      MS_ACL0,      MS_BTN3,      MS_BTN2,      MS_BTN1,                                  MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,      XXXXXXX,      MS_BTN5,
 	G_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         G_EQL,
 	KC_BSPC,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         KC_MINS,      KC_QUOT,
 	O_LSFT,       C_Z,          KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         A_COMM,       KC_DOT,       C_SLSH,       O_RSFT,
