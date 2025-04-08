158ca346480d1b29d1c84337600df7cb9b4bb3aa
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-04-08 12:46:10 +0200

    Not sure about this number layout.

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	G_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         G_EQL,
 	KC_BSPC,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         KC_MINS,      KC_QUOT,
 	O_LSFT,       C_Z,          KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         A_COMM,       KC_DOT,       C_SLSH,       O_RSFT,
-	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      O_L4,         O_L3,         KC_SPC,       KC_ENT,       O_L3,         O_L4,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      O_L4,         O_L3,         KC_SPC,       KC_ENT,       O_L3,         O_L4,         XXXXXXX,      XXXXXXX,      XXXXXXX,      MS_ACL0
   ),
 
   [L2] = LAYOUT(
@@ -126,10 +126,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L4] = LAYOUT(
 	XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      MS_BTN4,      MS_BTN5,                                  MS_WHLL,      MS_WHLD,      MS_WHLU,      MS_WHLR,      XXXXXXX,      XXXXXXX,
-	_______,      KC_0,         KC_9,         KC_8,         KC_7,         KC_6,                                     KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_F12,       KC_RGUI,
+	_______,      KC_6,         KC_7,         KC_8,         KC_9,         KC_0,                                     KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_F12,       KC_RGUI,
 	_______,      KC_5,         KC_4,         KC_3,         KC_2,         KC_1,                                     KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     KC_F11,       XXXXXXX,
 	KC_LSFT,      KC_LCTL,      KC_DEL,       KC_ESC,       XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSFT,
-	XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,      MO_L5,        KC_6,         KC_1,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
+	XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,      MO_L5,        KC_0,         KC_1,         XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
 
   [L5] = LAYOUT(
