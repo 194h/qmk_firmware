459be8d70b2d5604609d7b0f27123bba1bba2971
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-04-28 08:55:20 +0200

    something cool

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -101,7 +101,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L1] = LAYOUT(
-	XXXXXXX,      XXXXXXX,      MS_WHLL,      MS_WHLU,      MS_WHLD,      MS_WHLR,                                  MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,      MS_BTN1,      MS_BTN2,
+	MS_BTN1,      MS_BTN3,      MS_WHLL,      MS_WHLU,      MS_WHLD,      MS_WHLR,                                  MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,      XXXXXXX,      MS_BTN2,
 	G_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         G_EQL,
 	KC_BSPC,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         KC_MINS,      KC_QUOT,
 	O_LSFT,       C_Z,          KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         A_COMM,       KC_DOT,       C_SLSH,       O_RSFT,
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [L4] = LAYOUT(
-	XXXXXXX,      XXXXXXX,      MS_BTN4,      MS_BTN3,      MS_BTN3,      MS_BTN5,                                  MS_WHLL,      MS_WHLD,      MS_WHLU,      MS_WHLR,      XXXXXXX,      MS_BTN3,
+	MS_BTN4,      MS_BTN5,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  MS_WHLL,      MS_WHLD,      MS_WHLU,      MS_WHLR,      XXXXXXX,      MS_BTN3,
 	_______,      KC_0,         KC_9,         KC_8,         KC_7,         KC_6,                                     KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_F12,       KC_RGUI,
 	_______,      KC_5,         KC_4,         KC_3,         KC_2,         KC_1,                                     KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     KC_F11,       XXXXXXX,
 	KC_LSFT,      KC_LCTL,      KC_DEL,       KC_ESC,       XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      KC_COMM,      KC_DOT,       KC_SLSH,      KC_RSFT,
