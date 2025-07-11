4e57a9fea23421918688d16a04d7e57d2eac16fa
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2025-07-11 15:25:43 +0200

    I might regret this

--- keyboards/atreus62/keymaps/194h/keymap.c
+++ keyboards/atreus62/keymaps/194h/keymap.c
@@ -119,11 +119,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L1] = LAYOUT(
-	MS_BTN1,      KC_COPY,      KC_PASTE,     KC_FIND,      SS_DQTS,      SS_QTS,                                   MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,      MS_BTN3,      MS_BTN2,
+	MS_BTN1,      MS_BTN3,      KC_PASTE,     KC_COPY,      SS_DQTS,      SS_QTS,                                   MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,      KC_FIND,      MS_BTN2,
 	G_TAB,        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                     KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_RGUI,
 	KC_BSPC,      KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                     KC_H,         KC_J,         KC_K,         KC_L,         KC_MINS,      KC_QUOT,
 	O_LSFT,       KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                                     KC_N,         KC_M,         A_COMM,       KC_DOT,       C_SLSH,       O_RSFT,
-	KC_ESC,       KC_LCTL,      XXXXXXX,      XXXXXXX,      O_L4,         O_L3,         KC_SPC,       KC_ENT,       O_L3,         O_L4,         KC_RALT,      KC_EQL,       KC_SCLN,      KC_COLN
+	KC_ESC,       KC_LCTL,      MO_L5,        KC_RALT,      O_L4,         O_L3,         KC_SPC,       KC_ENT,       O_L3,         O_L4,         KC_RALT,      KC_EQL,       KC_SCLN,      KC_COLN
   ),
 
   [L2] = LAYOUT(
@@ -139,11 +139,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,      SS_QTS,       SS_DQTS,      KC_AT,        KC_HASH,      KC_TILD,                                  KC_QUES,      KC_EXLM,      KC_PIPE,      KC_AND,       XXXXXXX,      XXXXXXX,
     _______,      KC_ASTR,      KC_LBRC,      KC_PO,        KC_PC,        KC_RBRC,                                  KC_HAT,       KC_RCBR,      KC_LCBR,      KC_DLR,       KC_MINS,      KC_DQT,
     _______,      XXXXXXX,      KC_BSLS,      KC_PERC,      SS_CMNT,      KC_GRV,                                   NO_EN,        NO_EM,        KC_LT,        KC_GT,        _______,      _______,
-	XXXXXXX,      XXXXXXX,      SS_BRCS,      SS_POPC,      XXXXXXX,      XXXXXXX,      KC_UNDS,      SS_ENTV,      XXXXXXX,      XXXXXXX,      SS_CBRS,      _______,      XXXXXXX,      XXXXXXX
+	XXXXXXX,      XXXXXXX,      SS_BRCS,      SS_POPC,      XXXXXXX,      XXXXXXX,      KC_UNDS,      SS_ENTV,      XXXXXXX,      XXXXXXX,      SS_CBRS,      KC_PLUS,      XXXXXXX,      XXXXXXX
   ),
 
   [L4] = LAYOUT(
-	MS_BTN4,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  MS_WHLL,      MS_WHLD,      MS_WHLU,      MS_WHLR,      MS_BTN3,      MS_BTN5,
+	MS_BTN4,      MS_BTN3,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  MS_WHLL,      MS_WHLD,      MS_WHLU,      MS_WHLR,      XXXXXXX,      MS_BTN5,
 	_______,      KC_0,         KC_9,         KC_8,         KC_7,         KC_6,                                     KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       XXXXXXX,      KC_RGUI,
 	_______,      KC_5,         KC_4,         KC_3,         KC_2,         KC_1,                                     KC_LEFT,      KC_DOWN,      KC_UP,        KC_RIGHT,     XXXXXXX,      XXXXXXX,
 	KC_LSFT,      XXXXXXX,      KC_DEL,       KC_ESC,       XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      KC_COMM,      KC_DOT,       _______,      KC_RSFT,
@@ -153,8 +153,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L5] = LAYOUT(
     XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,
     TO_L1,        TO_L2,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  KC_F6,        KC_F7,        KC_F8,        KC_F9,        KC_F10,       XXXXXXX,
-    XXXXXXX,      XXXXXXX,      KC_PSCR,      XXXXXXX,      KC_WFAV,      KC_CALC,                                  KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,        XXXXXXX,
-    XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,                                  KC_F11,       KC_F12,       KC_LALT,      XXXXXXX,      KC_RCTL,      XXXXXXX,
+    XXXXXXX,      XXXXXXX,      KC_PSCR,      XXXXXXX,      KC_FIND,      KC_CALC,                                  KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,        XXXXXXX,
+    XXXXXXX,      QK_BOOT,      XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_WFAV,                                  KC_F11,       KC_F12,       KC_LALT,      XXXXXXX,      KC_RCTL,      XXXXXXX,
     XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX
   )
 
