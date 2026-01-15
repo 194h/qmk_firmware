5ee54d0b7732a614371ea7c992cc7f27a6a91e26
Henning Holmedal <henning.holmedal@basefarm-orange.com>
2026-01-15 11:08:34 +0100

    Trying to use the keyboard despite the broken touchpad.

--- keyboards/controllerworks/city42/keymaps/194h/keymap.c
+++ keyboards/controllerworks/city42/keymaps/194h/keymap.c
@@ -2,15 +2,15 @@
 
 // us altgr-weur specific aliases
 // Endash
-#define NO_EN RALT(KC_MINS)
+#define WEUR_EN RALT(KC_MINS)
 // Emdash
-#define NO_EM RALT(S(KC_MINS))
+#define WEUR_EM RALT(S(KC_MINS))
 // Norwegian ae
-#define NO_AE RALT(KC_W)
+#define WEUR_AE RALT(KC_W)
 // Norwegian oe
-#define NO_OE RALT(KC_T)
+#define WEUR_OE RALT(KC_T)
 // Norwegian aa
-#define NO_AA RALT(KC_Q)
+#define WEUR_AA RALT(KC_Q)
 
 //Some of the default aliases make no sense
 #define KC_AND KC_AMPERSAND
@@ -39,92 +39,104 @@
 #define L1 0
 #define L2 1
 #define L3 2
-#define L4 3
-#define L5 4
 
 // Momentary switch to layer
 #define MO_L3 MO(L3)
 #define MO_L4 MO(L4)
 #define MO_L5 MO(L5)
 // Momentary switch to layer - One Shot Layer
+#define OSL_L2 OSL(L2)
 #define OSL_L3 OSL(L3)
 #define OSL_L4 OSL(L4)
 #define OSL_L5 OSL(L5)
 //Toggle layers
 #define TO_L1 TO(L1)
-#define TO_L3 TO(L3)
-#define TO_L3 TO(L3)
-#define TO_L4 TO(L4)
 #define TO_L2 TO(L2)
+#define TO_L3 TO(L3)
 //L3 on hold, X on tap
 #define L3_X LT(L3, KC_X)
 //L3 on hold, . on tap
 #define L3_DOT LT(L3, KC_DOT)
 
+// Shift + Colon → Semicolon
+const key_override_t shift_colon_to_semicolon =
+    ko_make_basic(MOD_MASK_SHIFT, KC_COLN, KC_SCLN);
+
+// Shift + Slash → Equal
+const key_override_t shift_slash_to_equal =
+    ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, KC_EQL);
+
+// Shift + Minus → Plus
+const key_override_t shift_minus_to_plus =
+    ko_make_basic(MOD_MASK_SHIFT, KC_MINS, KC_PLUS);
+
+// Register all overrides
+const key_override_t *key_overrides[] = {
+    &shift_colon_to_semicolon,
+    &shift_slash_to_equal,
+    &shift_minus_to_plus,
+};
+
+const uint16_t PROGMEM combo5[] = {KC_4, KC_1, COMBO_END};
+const uint16_t PROGMEM combo5_1[] = {KC_3, KC_2, COMBO_END};
+const uint16_t PROGMEM combo6[] = {KC_4, KC_2, COMBO_END};
+const uint16_t PROGMEM combo6_1[] = {KC_3, KC_2, KC_1, COMBO_END};
+const uint16_t PROGMEM combo7[] = {KC_4, KC_3, COMBO_END};
+const uint16_t PROGMEM combo7_1[] = {KC_4, KC_2, KC_1, COMBO_END};
+const uint16_t PROGMEM combo8[] = {KC_4, KC_3, KC_1, COMBO_END};
+const uint16_t PROGMEM combo9[] = {KC_4, KC_3, KC_2, COMBO_END};
+const uint16_t PROGMEM combo_boot[] = {KC_4, KC_3, KC_2, KC_1, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(combo5, KC_5),
+    COMBO(combo5_1, KC_5),
+    COMBO(combo6, KC_6),
+    COMBO(combo6_1, KC_6),
+    COMBO(combo7, KC_7),
+    COMBO(combo7_1, KC_7),
+    COMBO(combo8, KC_8),
+    COMBO(combo9, KC_9),
+    COMBO(combo_boot, QK_BOOT),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L1] = LAYOUT_split_3x6_3(
 
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        G_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    G_EQL,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_RGUI,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_MINS, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       O_LSFT,     C_Z,    L3_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,  A_COMM,  L3_DOT,  C_SLSH,  O_RSFT,
+       O_LSFT,    KC_Z,    L3_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  L3_DOT, KC_SLSH,  O_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_ESC,  OSL_L4,  KC_SPC,     KC_ENT,  OSL_L4, KC_COLN
+                                          KC_LALT,  OSL_L2,  KC_SPC,     KC_ENT,  OSL_L2, KC_LCTL
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [L2] = LAYOUT_split_3x6_3(
-
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______,   NO_AA,
+      _______,  KC_EQL, XXXXXXX,   KC_AT, KC_HASH, KC_TILD,                      KC_QUES, KC_EXLM, KC_PIPE,  KC_AND, KC_PLUS, WEUR_AA,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______,   NO_OE,   NO_AE,
+      _______, KC_ASTR, KC_LBRC,   KC_PO,   KC_PC, KC_RBRC,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, WEUR_OE, WEUR_AE,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
+       KC_ESC, KC_BSLS,  KC_DEL, KC_PERC, XXXXXXX,  KC_GRV,                       KC_HAT, KC_RCBR, KC_LCBR,  KC_DLR, _______, KC_COLN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, _______, _______
+                                          _______, _______, KC_UNDS,    XXXXXXX, _______, _______
                                       //`--------------------------'  `--------------------------'
-
   ),
 
   [L3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX,   KC_AT, KC_HASH, KC_TILD,                      KC_QUES, KC_EXLM, KC_PIPE,  KC_AND, KC_PLUS,  KC_EQL,
+      _______,    KC_0,    KC_4,    KC_3,    KC_2,    KC_1,                      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, XXXXXXX, KC_RGUI,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_ASTR, KC_LBRC,   KC_PO,   KC_PC, KC_RBRC,                       KC_HAT, KC_RCBR, KC_LCBR,  KC_DLR, KC_MINS,  KC_DQT,
+      _______, MS_BTN4, MS_BTN1, MS_BTN3, MS_BTN2, MS_BTN5,                      MS_LEFT, MS_DOWN,   MS_UP, MS_RGHT, XXXXXXX, KC_RALT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_BSLS, KC_PERC, XXXXXXX,  KC_GRV,                        NO_EN,   NO_EM,   KC_LT,   KC_GT, XXXXXXX, _______,
+      KC_LSFT,  KC_F10,   KC_F4,   KC_F3,   KC_F2,   KC_F1,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, KC_UNDS,    XXXXXXX, _______, KC_SCLN
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [L4] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,    KC_0,    KC_9,    KC_8,    KC_7,    KC_6,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, KC_RGUI,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,    KC_5,    KC_4,    KC_3,    KC_2,    KC_1,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, KC_RALT,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_LCTL,  KC_DEL, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            MO_L5,    KC_6,    KC_1,    XXXXXXX, _______, _______
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [L5] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        TO_L1,   TO_L2, XXXXXXX, MS_WHLU, XXXXXXX, XXXXXXX,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN3, MS_BTN2, MS_BTN1,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, QK_BOOT, XXXXXXX, MS_WHLD, XXXXXXX, XXXXXXX,                       KC_F11,  KC_F12, KC_LALT, XXXXXXX, KC_RCTL, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, XXXXXXX, MS_BTN1,     KC_ENT, _______,   TO_L1
+                                          _______, _______,  KC_SPC,     KC_ENT, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
+
 
 };
 
@@ -142,12 +154,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
         case 2:  // L3 - White
             rgblight_sethsv_noeeprom(0, 0, 255);  // HSV White
-            break;
-        case 3:  // L4 - Blue
-            rgblight_sethsv_noeeprom(170, 255, 255); // HSV Blue
-            break;
-        case 4:  // L5 - Lime
-            rgblight_sethsv_noeeprom(75, 255, 255);   // HSV Lime
             break;
         default:
             rgblight_disable();
